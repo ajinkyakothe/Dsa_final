1f502797cda79a61c9247012564281f06d4bde4a
Ajinkya Kothe <115250148+ajinkyakothe@users.noreply.github.com>
2025-07-16 11:01:20 +0530

    Time: 0 ms (100.00%) | Memory: 35.9 MB (9.59%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n=customers.size();
-        
-        int maxUnSatCust=0;
-        int currUnSat=0;
+           
+          int n=customers.size();
 
-        for(int i=0;i<minutes;i++)
+          int maxUnSatCust=0,currUnnsat=0;
+
+          for(int i=0;i<minutes;i++)  //processing for the first window
+          {
+            currUnnsat += customers[i]*grumpy[i];
+          } 
+         
+          maxUnSatCust=currUnnsat; //currcust is for the updation and maxCust is for the customer
+         
+          int i=0;
+          int j=minutes;
+
+        while(j<n)
         {
-            currUnSat += customers[i]*grumpy[i];
+            currUnnsat += customers[j]*grumpy[j];
+            currUnnsat -= customers[i]*grumpy[i];
+
+            maxUnSatCust=max(maxUnSatCust,currUnnsat);
+            i++;
+            j++;
+
         }
+        
+       int totalCust=maxUnSatCust; //calculated using power
 
-        maxUnSatCust=currUnSat;
-
-        int i=0;
-        int j=minutes;
-
-      while(j<n)
-      {
-        currUnSat +=customers[j]*grumpy[j];
-        currUnSat -=customers[i]*grumpy[i];
-
-        maxUnSatCust=max(maxUnSatCust,currUnSat);
-        i++;
-        j++;
-      }
-
-      int totalSat=maxUnSatCust;//by using the power which is given in problem
-      for(int i=0;i<n;i++)
-      {
-        totalSat +=customers[i]*(1-grumpy[i]);
-      }
-
-     return totalSat;
+       //for O(n)
+ 
+       for(int i=0;i<n;i++)
+       {
+        totalCust += customers[i] * (1-grumpy[i]); 
+       }
 
 
-  
+     return totalCust;
 
+           
     }
 };
