62bdf4d9aa765d8feb1974a6baee1efb9c8843db
Ajinkya Kothe <115250148+ajinkyakothe@users.noreply.github.com>
2025-12-31 08:12:45 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (10.59%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,50 +1,52 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         
-         int ans[]={-1,-1};
+      int ans[]={-1,-1};
 
-          //for the first occurence
-         ans[0]=binarySearch(nums,target,true);
+     //for the first occurenece
+     ans[0]=binarySearch(nums,target,true);
 
-         //for the last occurence
-         ans[1]=binarySearch(nums,target,false);
+      //for the last occurence
+      ans[1]=binarySearch(nums,target,false);
 
-         return ans;
-          
+
+      return ans;
     }
-     static int binarySearch(int[] nums, int target, boolean findFirst) {
-       int s=0,e=nums.length-1;
-       int ans=-1;
 
-       while(s<=e)
-       {
-        int mid=s+(e-s)/2;
-    
-       if(target>nums[mid])
-       {
-        s=mid+1;
-       }
-       else if(target<nums[mid])
-      {
-        e=mid-1;
-      } 
-      else
-      {
-        ans=mid;
-        //for the first shrink window to left
-        if(findFirst)
+    static int binarySearch(int[] nums,int target,boolean findFirst)
+    {
+        int s=0, e=nums.length-1;
+
+        int ans=-1;
+
+        while(s<=e)
         {
-            e=mid-1;
+            int mid=s+(e-s)/2;
+
+            if(target > nums[mid])
+            {
+                s=mid+1;
+            }
+            
+            else if(target < nums[mid])
+            {
+                e=mid-1;
+            }
+
+            else
+            {
+                ans=mid;
+                if(findFirst)
+                {
+                    e=mid-1;
+                }
+                else
+                {
+                    s=mid+1;
+                }
+            }
         }
-        else
-        {
-            s=mid+1;
-        }
-      }
+        return ans;
+    }
 
-       }
-
-    return ans;
-
-     }
 }
