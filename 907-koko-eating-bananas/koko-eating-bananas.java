4048c4657e831b94726e52e0b83c786471123023
Ajinkya Kothe <115250148+ajinkyakothe@users.noreply.github.com>
2026-01-22 08:09:07 +0530

    Time: 6 ms (99.53%) | Memory: 48.2 MB (21.27%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,37 +1,38 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 1,high=0;
-
+         
+        int low=1, high=0;
         for(int pile:piles)
         {
-            high = Math.max(high,pile);
-        }
+            high=Math.max(high,pile);
+        }  
 
-        int result = 0;
-        
-        while(low <= high){
-            int mid = low + (high - low)/2;
+        int result=0;
+        while(low <= high)
+        {
+            int mid=low+(high-low)/2;
 
             if(calculateHours(piles,mid) <= h)
             {
-                result = mid;
-                high = mid-1;
+                result=mid;
+                high=mid-1;
             }
             else
             {
-                low = mid+1;
+                low=mid+1;
             }
+          
         }
-        return result;
+
+         return result;
     }
 
     private long calculateHours(int[] piles,int k)
     {
         long hours=0;
-
         for(int pile:piles)
         {
-            hours += (pile + k -1)/k;
+            hours += (pile+k-1)/k;
         }
         return hours;
     }
